db16caa936cda4ad5ac148532202b7c802c67955
Muhammadjon <exampple@gmail.com>
2024-09-17 18:52:50 +0500

    data from api

--- web/urls.py
+++ web/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
-from .views import (Index_View, Login_View, Register_View, Log_OutView, Team_View, Testimonial_View, Service_View, Offer_View,
-                    Feature_View, FAQ_View, Contact_View, Blog_View, Course_View, Error_View, CoursedetailView)
+from .views import (Index_View, Login_View, Register_View)
 
 urlpatterns = [
     path('', Index_View.as_view(), name='index'),
     path('login/', Login_View.as_view(), name='login'),
-    path('register/', Register_View.as_view(), name='register'),
-    path('logout/', Log_OutView.as_view(), name='logout')
+    path('register/', Register_View.as_view(), name='register')
 ]
